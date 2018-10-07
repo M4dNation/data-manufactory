5cd4d2d4a8352680f6d004550ab2782eb730dd79
Axel Vaindal <pro.axelvaindal@gmail.com>
2018-10-07 18:37:50 +0200

    Implement factory build function

---
+++ factory.js
@@ -0,0 +1,62 @@
+import { Obj } from "jstoolbox";
+
+export default Factory
+{
+    constructor({name = "factory", schema = {}, after = [], afterBuild = (() => {})})
+    {
+        if (!Obj.isString(name) || Obj.isFalsy(name))
+            throw new Error("Factory name must be a nom empty string");
+
+        if (!Obj.isPlainObject(schema))
+            throw new Error("schema must be a plain object");    
+
+        if (!Obj.isArray(after))
+            throw new Error("after must be an array");
+
+        if (!Obj.isFunction(afterBuild))
+            throw new Error("afterBuild must be a function")
+
+        this.name = name;
+        this.schema = schema;
+        this.after = after;
+        this.afterBuild = afterBuild;
+    }
+
+    build(count = 1)
+    {
+        count = count < 1 ? 1 : count;
+
+        if (!Obj.isPlainObject(options))
+            throw new Error("options must be a plain object");
+
+        let result = [];
+
+        for (let i = 0; i < count; i++)
+        {
+            let obj = {};
+
+            for (const [index, value] of Object.entries(this.schema))
+            {
+                if (Obj.isFunction(value))
+                    obj[index] = value();
+                else obj[index] = value;
+            }
+
+            for (const after of this.after)
+            {
+                after(obj);
+            }
+
+            result.push(obj);
+        }
+
+        this.afterBuild(result);
+
+        return result;
+    }
+
+    extend({name = "extended factory", schema = {}, after = []})
+    {
+
+    }
+};
